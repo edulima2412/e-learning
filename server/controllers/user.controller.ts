9457b07769a549451bda58f814f2760e4e771a81
Carlos Eduardo Lima Rocha <edulima2412@gmail.com>
2023-09-22 12:49:18 -0300

    created logout controller

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -151,3 +151,18 @@ export const loginUser = CatchAsyncError(async (req: Request, res: Response, nex
         return next(new ErrorHandler(error.message, 400))
     }
 })
+
+// logout user
+export const logoutUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.cookie("access_token", "", { maxAge: 1 })
+        res.cookie("refresh_token", "", { maxAge: 1 })
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
