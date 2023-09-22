907034e1f75c2c7ad94b6fd927d0dddd62b9b28e
Carlos Eduardo Lima Rocha <edulima2412@gmail.com>
2023-09-22 07:20:31 -0300

    initial commit

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import connectDB from "./utils/db";
+require("dotenv").config()
+
+// Create Server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is connected with pront ${process.env.PORT}`)
+    connectDB()
+})
