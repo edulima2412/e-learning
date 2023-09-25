a571b3ed6771819376689ece82b1b7473eef912e
Carlos Eduardo Lima Rocha <edulima2412@gmail.com>
2023-09-25 08:07:18 -0300

    update course

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router()
 
 courseRouter.post('/course', isAuthenticated, authorizeRoles('admin'), uploadCourse)
+
+courseRouter.put('/course/:id', isAuthenticated, authorizeRoles('admin'), editCourse)
 
 export default courseRouter
